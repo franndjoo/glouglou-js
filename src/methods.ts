f7246b440c1063d769b2e29b6f007d2f9419a03a
Johan Montorfano <me@johanmontorfano.com>
2023-05-11 00:52:49 +0200

    added support for custom parameters

--- src/methods.ts
+++ src/methods.ts
@@ -2,25 +2,26 @@ import { Configuration, Email } from "./interfaces";
 import { composeURL, sendRequest } from "./utils";
 
 /** Used to send an email */
-export async function sendEmail(config: Configuration, email: Email) {
+export async function sendEmail(config: Configuration, email: Email, optionnalParams?: {[entry: string]: any}) {
     const composedURL = composeURL(config, "send", {
         "api-key": config.apiKey,
         "to": `${email.recipient.name} <${email.recipient.email}>`,
         "subject": email.subject,
         "body": email.body,
-        ...(email.cc !== undefined ? { "cc": `${email.cc.name} <${email.cc.email}>` } : "")
+        ...(email.cc !== undefined ? { "cc": `${email.cc.name} <${email.cc.email}>` } : ""),
+        ...optionnalParams
     });
     return await sendRequest(composedURL);
 }
 
 /** Used to verify the validity if the given API key */
-export async function verifyAPIKey(config: Configuration) {
-    const composedURL = composeURL(config, "access-ok", { "api-key": config.apiKey });
+export async function verifyAPIKey(config: Configuration, optionnalParams?: {[entry: string]: any}) {
+    const composedURL = composeURL(config, "access-ok", { "api-key": config.apiKey, ...optionnalParams });
     return await sendRequest(composedURL);
 }
 
 /** Used to verify that the remote GlouGlou instance is reachable */
-export async function pingInstance(config: Configuration) {
-    const composedURL = composeURL(config, "ping");
+export async function pingInstance(config: Configuration, optionnalParams?: {[entry: string]: any}) {
+    const composedURL = composeURL(config, "ping", optionnalParams);
     return await sendRequest(composedURL);
 }
